5191dcf6c53f3c6c20157e0634431e4456b4a549
BAlvernaz <balverna@gmail.com>
2019-07-08 15:49:16 -0700

    Server Creation and Get Routes Created

--- backend/server/server.js
+++ backend/server/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
-const app = express()
-const PORT = process.env.PORT || 3000
+const app = express();
+const PORT = process.env.PORT || 3000;
+const path = require('path');
 
+app.use(express.static(path.join(__dirname, '../../public')))
+app.use(express.json());
+app.use(express.urlencoded());
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+app.use('/api/students', require('./routes/students'));
+app.use('/api/schools', require('./routes/schools'));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '../../public', 'index.html'));
+});
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+
+module.exports = app;
