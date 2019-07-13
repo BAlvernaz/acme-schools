11d006f9c814afeed4eab0d04969800d29307b3d
BAlvernaz <balverna@gmail.com>
2019-07-12 17:12:31 -0700

    Changes Made

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from 'react';
+import { connect } from 'react-redux';
+import { addStudent } from '../store';
 
 class Form extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      gpa: '',
+      schoolId: ''
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(ev) {
+    this.setState({ [ev.target.name]: ev.target.value });
+  }
+
   render() {
-    return <h1>TODO FORM</h1>
+    const { firstName, lastName, email, gpa, schoolId } = this.state;
+    const { students, schools, onSubmit } = this.props;
+    const { onChange } = this;
+    console.log(this.state);
+    return (
+      <form>
+        <label htmlFor="firstName" className="inputLables">
+          First Name <input name="firstName" value={firstName} onChange={onChange} />
+        </label>
+        <label htmlFor="lastName" className="inputLables">
+          Last Name <input name="lastName" value={lastName} onChange={onChange} />
+        </label>
+        <label htmlFor="email" className="inputLables">
+          Email <input name="email" value={email} onChange={onChange} />
+        </label>
+        <label htmlFor="gpa" className="inputLables">
+          GPA <input name="gpa" value={gpa} onChange={onChange} />
+        </label>
+        <label htmlFor="schoolId">
+          Enroll At{' '}
+          <select name="schoolId" onChange={onChange} value={schoolId}>
+            <option value="null">--Not Enrolled --</option>
+            {schools.map(school => (
+              <option key={school.id} value={school.id}>
+                {school.name}
+              </option>
+            ))}
+          </select>
+        </label>
+      </form>
+    );
   }
 }
 
-export default Form
+const stateToProps = state => state;
+
+const distpatchToProps = dispatch => {
+  return {
+    onSubmit: (student)=> dispatch(addStudent(student))
+  }
+}
+
+export default connect(stateToProps, distpatchToProps)(Form);
--- src/store.js
+++ src/store.js
@@ -1,10 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import axios from 'axios';
-import thunkMiddleware from 'redux-thunks'
+import thunk from 'redux-thunk'
 
 const GET_ALL = 'GET_ALL';
 const GOING_TO_SCHOOL = 'GOING_TO_SCHOOL'
+const NEW_STUDENT = 'NEW_STUDENT'
 
+const newStudent = (student) => ({
+  type: NEW_STUDENT,
+  student
+})
+
+export const addStudent = (student) => {
+  
+  return async(dispatch) => {
+    const response = await axios.post('/api/students', student)
+    dispatch(newStudent(response.data))
+
+  }
+}
 
 const initState = {
   students: [],
@@ -22,10 +36,15 @@ const reducer = (state = initState, action) => {
         ? {...student, schooId: action.schooId}
         : student
       })
-      return {...state, students}
+      state = {...state, students}
+      break;
+    case NEW_STUDENT:
+      state = {...state, students: [...state.students, action.product]}
   }
   return state;
 };
+
+const store = createStore(reducer, applyMiddleware(thunk));
 
 const start = async () => {
   const students = axios.get('/api/students');
@@ -39,7 +58,5 @@ const start = async () => {
 };
 
 start();
-
-const store = createStore(reducer, applyMiddleware(thunkMiddleware));
 
 export default store;
